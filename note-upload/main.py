4aea31aa1e22a2d3e92d1bf619e2ffd52d9ec71c
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2024-03-24 13:53:06 -0700

    note uploading

--- note-upload/main.py
+++ note-upload/main.py
@@ -1,7 +1,27 @@
+import markdown
+import sys
+import datetime
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("blog-fcb54-firebase-adminsdk-l7q62-5879920725.json")
-app = firebase_admin.initialize_app(cred)
-db = firestore.client()
-print(db)
+# TODO: get path to note
+note_path = sys.argv[1]
+
+with open(note_path, "r") as file:
+
+    # get note content and convert to html
+    note = file.read()
+    title = note.split("\n")[0].strip("# ") # get title from first line
+    html = markdown.markdown(note)
+    date = datetime.datetime.now().isoformat()
+    doc_id = title.replace(" ", "-").lower() + "-" + date.split("T")[0]
+    
+    # connect to firebase
+    cert_location = "/Users/sophiasharif/Desktop/projects/blog/note-upload/blog-fcb54-firebase-adminsdk-l7q62-5879920725.json"
+    cred = credentials.Certificate(cert_location)
+    app = firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    doc_ref = db.collection('users').document(doc_id)
+    doc_ref.set({"title": title, "date": date, "content": html}) # TODO: tags
+
+# TODO: add exit statuses

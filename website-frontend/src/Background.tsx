71f470acdc36a2e0959a6909a0671978919648bd
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2024-05-03 10:01:57 -0700

    overlay circle + refactoring

--- website-frontend/src/Background.tsx
+++ website-frontend/src/Background.tsx
@@ -1,13 +1,19 @@
 import anime from "animejs";
 
-const getColor = (() => {
+const colorPicker = (() => {
   const colors = ["#FF6138", "#FFBE53", "#2980B9", "#282741"];
   let index = 0;
-  function getColor() {
-    index += 1;
+  function current() {
     return colors[index % colors.length];
   }
-  return getColor;
+  function next() {
+    index += 1;
+    return current();
+  }
+  return {
+    current,
+    next,
+  };
 })();
 
 class Circle {
@@ -16,13 +22,7 @@ class Circle {
   y: number;
   targetR: number;
 
-  constructor(
-    x: number,
-    y: number,
-    targetR: number,
-    duration: number,
-    color: string
-  ) {
+  constructor(x: number, y: number, targetR: number, color: string) {
     // update attributes
     this.x = x;
     this.y = y;
@@ -39,23 +39,59 @@ class Circle {
 
     // add the element to the background
     document.getElementById("background")!.appendChild(this.element);
-
-    // animate the element
-    anime({
-      targets: this.element,
-      scale: [0, 1],
-      duration: duration,
-      complete: () => {
-        this.element.remove();
-        document.getElementById("background")!.style.backgroundColor = color;
-      },
-    });
   }
 }
 
+function backgroundCircle(
+  e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  color: string
+) {
+  const x = e.clientX;
+  const y = e.clientY;
+  const w = Math.max(window.innerWidth - x, x);
+  const h = Math.max(window.innerHeight - y, y);
+  const targetR = Math.sqrt(Math.pow(w, 2) + Math.pow(h, 2)) * 2;
+  const backgroundCircle = new Circle(x, y, targetR, color);
+  const duration = targetR / 2;
+  anime({
+    targets: backgroundCircle.element,
+    scale: [0, 1],
+    duration: duration,
+    easing: "easeOutQuart",
+    complete: () => {
+      backgroundCircle.element.remove();
+      document.getElementById("background")!.style.backgroundColor = color;
+    },
+  });
+  return duration; // return the duration so that the overlay circle can use it
+}
+
+function overlayCircle(
+  e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  color: string,
+  duration: number
+) {
+  const x = e.clientX;
+  const y = e.clientY;
+  const targetR = Math.max(window.innerWidth, window.innerHeight) / 3;
+  const backgroundCircle = new Circle(x, y, targetR, color);
+  anime({
+    targets: backgroundCircle.element,
+    scale: [0, 1],
+    opacity: [1, 0],
+    duration: duration,
+    easing: "easeOutExpo",
+    complete: () => {
+      backgroundCircle.element.remove();
+    },
+  });
+}
+
 function updateBackground(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-  new Circle(e.clientX, e.clientY, window.innerWidth * 2, 4000, getColor());
-  //   new Circle(e.clientX, e.clientY, 100, 1000, getColor());
+  const currColor = colorPicker.current();
+  const nextColor = colorPicker.next();
+  const duration = backgroundCircle(e, nextColor);
+  overlayCircle(e, currColor, duration);
 }
 
 function Background() {
